6ad914e14a3b4e590b63b064502ca3f2fc9eb5c2
JcogolloHernandez12 <josandres123@gmail.com>
2024-06-04 11:22:53 -0500

    Embedded Audit

--- src/main/java/com/andres/curso/springboot/jpa/springbootjpa/entities/Person.java
+++ src/main/java/com/andres/curso/springboot/jpa/springbootjpa/entities/Person.java
@@ -1,6 +1,7 @@
 package com.andres.curso.springboot.jpa.springbootjpa.entities;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -20,6 +21,9 @@ public class Person {
 
     @Column(name = "programming_language")
     private String programmingLanguage;
+
+    @Embedded
+    private Audit audit = new Audit();
 
     public Person() {
     }
@@ -64,7 +68,7 @@ public class Person {
     @Override
     public String toString() {
         return "[id=" + id + ", name=" + name + ", lastname=" + lastname + ", programmingLanguage="
-                + programmingLanguage + "]";
+                + programmingLanguage + ", createAt=" + audit.getCreatAt() + ", updated=" + audit.getUpdatedAt() + "]";
     }
 
     
